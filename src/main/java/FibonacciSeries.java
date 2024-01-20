dfe55b176a6e2250020ff0f8c47d39eb8f06b8e7
dollytiwari <dollytiwari393@gmail.com>
2024-01-20 21:20:29 +0530

    commit

--- src/main/java/FibonacciSeries.java
+++ src/main/java/FibonacciSeries.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciSeries {
 
     public void seriesPrint() {
@@ -13,13 +16,18 @@ public class FibonacciSeries {
             num1 = num2;
             System.out.print(" " + num3);
         }*/
+        List<Integer> list = new ArrayList<>();
         System.out.print(num1 + " " + num2);
         int num3 = num1 + num2;
+        list.add(num1);
+        list.add(num2);
         while(num3<=1000){
-            System.out.print(" "+num3);
+            list.add(num3);
+            System.out.print(" "+list);
             num1 = num2;
             num2 = num3;
             num3=num1+num2;
+
 
         }
         /*  System.out.print(" "+num3);*/
