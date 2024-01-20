bd384de18a95b36b2b647c7f6558c5181dbdf52e
dollytiwari <dollytiwari393@gmail.com>
2024-01-20 20:37:05 +0530

    Initial commit

---
+++ src/main/java/FibonacciSeriesTest.java
@@ -0,0 +1,8 @@
+public class FibonacciSeriesTest {
+
+    public static void main(String[] args){
+
+        new FiboncciSeries().seriesPrint();
+
+    }
+}
---
+++ src/main/java/FiboncciSeries.java
@@ -0,0 +1,16 @@
+public class FiboncciSeries {
+
+    public void seriesPrint(){
+
+        int num1= 0, num2=1, i;
+
+        System.out.print(num1+ " "+ num2);
+
+        for(i=2; i<10; i++){
+            int num3 = num1+num2;
+            num2=num3;
+            num1=num2;
+            System.out.print( " "+num3);
+        }
+    }
+}
