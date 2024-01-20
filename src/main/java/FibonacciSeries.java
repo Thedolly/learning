2d6c7c4cff84b3cb157810e71d1939d1b8f28697
dollytiwari <dollytiwari393@gmail.com>
2024-01-20 23:20:41 +0530

    commit

--- src/main/java/FibonacciSeries.java
+++ src/main/java/FibonacciSeries.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class FibonacciSeries {
 
@@ -13,17 +14,18 @@ public class FibonacciSeries {
         int num3 = num1 + num2;
         list.add(num1);
         list.add(num2);
-        while (num3 <= 1000) {
+        while (num3 <= 10000) {
             list.add(num3);
             num1 = num2;
             num2 = num3;
             num3 = num1 + num2;
         }
-        System.out.print(" " + list);
-        System.out.println();
+        System.out.println("febonacci series " + list);
         Random random = new Random();
         int index = random.nextInt(0, 17);
-        System.out.println(" " + list.get(index));
+        System.out.println("random number from fibonacci series " + list.get(index));
+        System.out.print("even number from fibonacci series  " + list.stream().filter(i -> (i % 2) == 0).collect(Collectors.toList()));
+
 
     }
 }
