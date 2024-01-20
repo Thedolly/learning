58c1c5817fd06976c32b3ef7e94bc7f41d32cecb
dollytiwari <dollytiwari393@gmail.com>
2024-01-20 22:18:15 +0530

    commit

--- src/main/java/FibonacciSeries.java
+++ src/main/java/FibonacciSeries.java
@@ -13,17 +13,17 @@ public class FibonacciSeries {
         int num3 = num1 + num2;
         list.add(num1);
         list.add(num2);
-        while(num3<=1000){
+        while (num3 <= 1000) {
             list.add(num3);
             num1 = num2;
             num2 = num3;
-            num3=num1+num2;
+            num3 = num1 + num2;
         }
-        System.out.print(" "+list);
+        System.out.print(" " + list);
         System.out.println();
         Random random = new Random();
-        int index =random.nextInt(0,17);
-        System.out.println(" " +list.get(index));
+        int index = random.nextInt(0, 17);
+        System.out.println(" " + list.get(index));
 
     }
 }
