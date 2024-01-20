ad00b5f86a213e014fe41f621c5f2215630953cd
dollytiwari <dollytiwari393@gmail.com>
2024-01-20 20:39:44 +0530

    commit

--- src/main/java/FiboncciSeries.java
+++ src/main/java/FiboncciSeries.java
@@ -1,16 +1,18 @@
 public class FiboncciSeries {
 
-    public void seriesPrint(){
+    public void seriesPrint() {
 
-        int num1= 0, num2=1, i;
+        int num1 = 0;
+        int num2 = 1;
+        int i;
 
-        System.out.print(num1+ " "+ num2);
+        System.out.print(num1 + " " + num2);
 
-        for(i=2; i<10; i++){
-            int num3 = num1+num2;
-            num2=num3;
-            num1=num2;
-            System.out.print( " "+num3);
+        for (i = 2; i < 10; i++) {
+            int num3 = num1 + num2;
+            num2 = num3;
+            num1 = num2;
+            System.out.print(" " + num3);
         }
     }
 }
