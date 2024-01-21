29ab280269229bafcdb365557639dc67e36a373c
dollytiwari <dollytiwari393@gmail.com>
2024-01-21 17:22:30 +0530

    commit

--- src/main/java/EmployeeTest.java
+++ src/main/java/EmployeeTest.java
@@ -7,7 +7,9 @@ public class EmployeeTest {
 
         List<String> list = Arrays.asList("James", "Jenifer", "Nick", "Joe", "Harrick", "Parth", "Timcook");
 
-        Collections.sort(list, Comparator.comparingInt(String::length));
+      //  Collections.sort(list, Comparator.comparingInt(String::length));
+
+        list.sort(Comparator.comparingInt(String::length));
 
         for (String str : list) {
             System.out.print(str + " ");
